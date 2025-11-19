28bb189d20c85d51f787f46c57537b9370fae4c3
Vittoria <vittoriatosi2006@gmail.com>
2025-11-19 13:16:19 +0100

    esercizio 4 svolto

---
+++ terzoEsercizio/terzoEsercizio.js
@@ -0,0 +1,8 @@
+function addTask() {
+  const list = document.querySelector("ul");
+  const input = document.querySelector("input");
+}
+
+const li = document.createElement("li");
+
+const checkbox = document.createElement("input");
